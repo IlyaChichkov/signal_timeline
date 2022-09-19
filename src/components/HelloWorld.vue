57ff8edb6c022e50d5d66b6b0e8be2d6a821d215
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2022-09-19 23:33:23 +0300

    Init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,244 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold mb-10">Signals encodings</h1>
+    <div class="flex flex-col justify-center items-center mb-4">
+      <canvas class="object-center" id="canvas" ref="maincanvas" :width="width" :height="height"
+              @mousemove="showCoordinates"></canvas>
+    </div>
+    <input @change.prevent="handleInputString($event)" class="border-solid border-2 border-gray-200 rounded p-2"
+           type="text" placeholder="0101001110">
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      canvas: null,
+      bufferCanvas: null,
+      width: 800,
+      height: 100,
+      stepsCount: 13,
+      stepHeight: 20,
+      x: 0,
+      y: 0,
+      sizeChanged: false,
+      inputString: '',
+      inputSignal: [0, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+      signals: []
+    }
+  },
+  computed: {
+    getStepWidth() {
+      return this.width / this.stepsCount;
+    },
+    getCanvas() {
+      return document.getElementById('canvas');
+    },
+    ctx() {
+      return this.getCanvas.getContext('2d');
+    },
+    visibleSegmentsCount() {
+      return this.width / this.getStepWidth;
+    }
+  },
+  methods: {
+    showCoordinates(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    updateCanvasWidth() {
+      this.width = document.documentElement.clientWidth / 2;
+      this.sizeChanged = true;
+    },
+    draw() {
+      this.ctx.clearRect(0, 0, this.width, this.height);
+      this.drawTimeline();
+      this.drawClockPeriodLines();
+      this.drawYAxis();
+      this.sizeChanged = false;
+    },
+    handleInputString(e) {
+      this.inputSignal = [];
+      console.log(e.target.value);
+      let input = e.target.value;
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] == 1 || input[i] == 0)
+          this.inputSignal.push(input[i]);
+      }
+
+      this.createSignals('UniP-RZ');
+      this.stepsCount = this.inputSignal.length;
+      this.draw();
+      this.sizeChanged = true;
+    },
+    drawTimeline() {
+      for (let i = 0; i < this.visibleSegmentsCount; i++) {
+        let vol = this.signals[i];
+        let nextStartVol = this.signals[i + 1];
+        if (!vol) {
+          vol = 0;
+        }
+        if (!nextStartVol) {
+          nextStartVol = 0;
+        }
+        this.drawSegment(i, vol, nextStartVol);
+      }
+    },
+    // eslint-disable-next-line no-unused-vars
+    drawSegment(index, vol, nextStartVol) {
+      let lineWidth = 2;
+      let startVol, centerLeftVol, centerRightVol, endVol;
+      nextStartVol = -nextStartVol[0];
+      startVol = -vol[0];
+      centerLeftVol = -vol[1];
+      centerRightVol = -vol[2];
+      endVol = -vol[3];
+
+      const startX = index * this.getStepWidth;
+      const endX = startX + this.getStepWidth;
+      const centerX = startX + (endX - startX) / 2;
+      const quarter = (endX - startX) / 4;
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(startX, this.height / 2 + startVol * this.stepHeight);
+      this.ctx.lineTo(startX + quarter, this.height / 2 + startVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      // Center left vertical
+      this.ctx.beginPath();
+      this.ctx.moveTo(startX + quarter, this.height / 2 + startVol * this.stepHeight);
+      this.ctx.lineTo(startX + quarter, this.height / 2 + centerLeftVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(centerX - quarter, this.height / 2 + centerLeftVol * this.stepHeight);
+      this.ctx.lineTo(centerX, this.height / 2 + centerLeftVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      // Center vertical
+      this.ctx.beginPath();
+      this.ctx.moveTo(centerX, this.height / 2 + centerLeftVol * this.stepHeight);
+      this.ctx.lineTo(centerX, this.height / 2 + centerRightVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(centerX, this.height / 2 + centerRightVol * this.stepHeight);
+      this.ctx.lineTo(centerX + quarter, this.height / 2 + centerRightVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      // Center right vertical
+      this.ctx.beginPath();
+      this.ctx.moveTo(centerX + quarter, this.height / 2 + centerRightVol * this.stepHeight);
+      this.ctx.lineTo(centerX + quarter, this.height / 2 + endVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(centerX + quarter, this.height / 2 + endVol * this.stepHeight);
+      this.ctx.lineTo(endX, this.height / 2 + endVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(endX, this.height / 2 + endVol * this.stepHeight);
+      this.ctx.lineTo(endX, this.height / 2 + nextStartVol * this.stepHeight);
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.stroke();
+    },
+    setRandomSignals() {
+      for (let i = 0; i < this.visibleSegmentsCount; i++) {
+        let vol = Math.round(Math.random() * 2 - 1);
+        this.inputSignal[i] = vol;
+      }
+    },
+    drawClockPeriodLines() {
+      for (let i = 0; i < this.visibleSegmentsCount; i++) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(i * this.getStepWidth, 0);
+        this.ctx.lineTo(i * this.getStepWidth, this.height);
+        this.ctx.lineWidth = 0.5;
+        this.ctx.stroke();
+
+        let signal = this.inputSignal[i];
+        console.log('>>> P ' + signal + ' i=' + i)
+        if (!signal) signal = 0;
+        this.ctx.fillText(signal.toString(), i * this.getStepWidth + this.getStepWidth / 2 - 2, 20);
+      }
+    },
+    drawYAxis() {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, this.height / 2);
+      this.ctx.lineTo(this.width, this.height / 2);
+      this.ctx.lineWidth = 0.2;
+      this.ctx.stroke();
+    },
+    createSignals(encodeType) {
+      switch (encodeType) {
+        case 'UniP-RZ':
+          this.signals = this.unipolarRZCode(this.inputSignal);
+      }
+    },
+    unipolarRZCode(inputSignal) {
+      let signals = []
+      for (let i = 0; i < inputSignal.length; i++) {
+        let currentBit = inputSignal[i];
+        console.log(i + '  ' + currentBit)
+        if (currentBit == 1) {
+          signals[i] = [1, 1, 0, 0];
+        } else {
+          signals[i] = [0, 0, 0, 0];
+        }
+        console.log('transform to ' + signals[i])
+      }
+      return signals;
+    },/*
+    unipolarRZCode(inputSignal){
+      let signals = []
+      for (let i = 0; i < inputSignal.length; i++) {
+        let prevBit = inputSignal[i - 1];
+        let currentBit = inputSignal[i];
+        let nextBit = inputSignal[i + 1];
+        if(!prevBit) prevBit = 0;
+        if(!nextBit) nextBit = 0;
+        if(currentBit == 1 && nextBit == 1){
+          signals[i] = [1, 1, 1];
+        }
+      }
+      return signals;
+    }*/
+  },
+  mounted() {
+    this.createSignals('UniP-RZ');
+    this.stepsCount = this.inputSignal.length;
+
+    this.updateCanvasWidth();
+    window.addEventListener('resize', () => {
+      this.updateCanvasWidth();
+    }, true)
+  },
+  updated() {
+    if (this.sizeChanged) {
+      this.draw();
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+#canvas {
+  border: 2px solid grey;
+  stroke: black;
+  stroke-width: 16px;
+  fill: oldlace;
 }
 </style>
